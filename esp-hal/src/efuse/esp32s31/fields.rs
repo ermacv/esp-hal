5515e50d8a01647c2839508b5056c3136f1c41c2
ermacv <ermachenkov.v.s@gmail.com>
2026-07-12 20:49:22 +0200

    Fix ESP32-S31 factory MAC eFuse field

--- esp-hal/src/efuse/esp32s31/fields.rs
+++ esp-hal/src/efuse/esp32s31/fields.rs
@@ -4,4 +4,4 @@ use crate::efuse::EfuseField;
 
 // ESP-IDF esp_efuse_table.csv: MAC_FACTORY occupies BLK1 bits 0..47.
 pub const MAC0: EfuseField = EfuseField::new(1, 0, 0, 32);
-pub const MAC1: EfuseField = EfuseField::new(1, 1, 0, 16);
+pub const MAC1: EfuseField = EfuseField::new(1, 1, 32, 16);
