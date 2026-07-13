68934aad217e23e59b9a4cccd63a90b611c9b129
ermacv <ermachenkov.v.s@gmail.com>
2026-07-13 10:04:36 +0200

    Test ESP32-S31 LP APM lower channels

--- esp-hal/src/gmac.rs
+++ esp-hal/src/gmac.rs
@@ -45,13 +45,13 @@ fn cnnt_sys_regs() -> &'static crate::pac::cnnt_sys::RegisterBlock {
 #[inline]
 fn disable_apm_filters() {
     let lp_apm = unsafe { &*crate::pac::LP_APM::ptr() };
-    lp_apm.func_ctrl().write(|w| unsafe { w.bits(0) });
+    lp_apm.func_ctrl().write(|w| unsafe { w.bits(0b1100) });
 
     let hp_apm = unsafe { &*crate::pac::HP_APM::ptr() };
     hp_apm.func_ctrl().write(|w| unsafe { w.bits(0) });
 
     let hp_mem_apm = unsafe { &*crate::pac::HP_MEM_APM::ptr() };
-    hp_mem_apm.func_ctrl().reset();
+    hp_mem_apm.func_ctrl().write(|w| unsafe { w.bits(0) });
 }
 
 const BUFFER_SIZE: usize = 1536;
